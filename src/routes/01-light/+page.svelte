8bf1fa829747d78b45114835aecb918680e6b185
Ty Fiero <tyfierodev@gmail.com>
2024-02-28 16:24:38 -0800

    2nd version

--- src/routes/01-light/+page.svelte
+++ src/routes/01-light/+page.svelte
@@ -2,6 +2,7 @@
 	import Icon from '@iconify/svelte';
 	import { fade } from 'svelte/transition';
 	import SvelteSeo from 'svelte-seo';
+	let loaded = false; // Tracks if the image has loaded
 </script>
 
 <SvelteSeo
@@ -34,48 +35,114 @@
 	}}
 />
 
-<div class="flex h-screen flex-col !overflow-hidden lg:flex-row">
-	<div class="relative w-full flex-1 overflow-y-auto lg:w-1/2">
-		<div class="absolute top-20 flex w-full items-center justify-center">
+<div class="relative flex min-h-screen flex-col">
+	<div class="absolute left-0 top-0 -z-10 h-full w-1/2 bg-white"></div>
+	<div class="absolute right-0 top-0 -z-10 hidden h-full w-1/2 bg-black md:block"></div>
+
+	<!-- SECTION 1 -->
+	<div class="flex w-full flex-col pt-20 md:flex-row">
+		<!-- LEFT -->
+		<div class="w-full md:w-1/2">
 			<h1 class="text-center text-5xl">01 Light</h1>
 		</div>
-		<div class="h-[40rem] w-full">
-			<img
-				in:fade={{ duration: 400 }}
-				class="fade-fast h-auto w-full"
-				src="/hand.png"
-				alt="01 light"
-			/>
-		</div>
-		<div class="flex w-full justify-center">
-			<a
-				class="z-5 mt-4 h-min cursor-pointer rounded-full border border-black px-3 py-2 text-black transition hover:bg-black hover:text-white"
-				href="https://0ggfznkwh4j.typeform.com/to/UvODRiEO"
-				target="_blank"
-				rel="noopener noreferrer">Pre-Order</a
-			>
+		<!-- RIGHT -->
+		<div class="w-full md:w-1/2">
+			<h1 class="text-center text-5xl text-white">Developer Community</h1>
 		</div>
 	</div>
-	<div class="relative w-full flex-1 overflow-y-auto bg-black lg:w-1/2">
-		<h1 class="my-40 text-center text-5xl text-white">Join the Developer Community</h1>
 
-		<div class="flex w-full items-center justify-center gap-4">
-			<a
-				class="z-5 mt-4 flex h-min cursor-pointer items-center gap-2 border border-white px-3 py-2 text-white transition hover:bg-white hover:text-black"
-				href="https://discord.gg/Hvz9Axh84z"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<Icon icon="ic:round-discord" class="h-5 w-5" /> Join the Community
-			</a>
-			<a
-				class="z-5 mt-4 flex h-min cursor-pointer items-center gap-2 border border-white px-3 py-2 text-white transition hover:bg-white hover:text-black"
-				href="https://github.com/KillianLucas/01"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<Icon icon="fa:github" class="h-5 w-5" /> Github
-			</a>
+	<div class="flex w-full flex-col md:flex-row">
+		<!-- LEFT -->
+		<div class="w-full md:w-1/2">
+			<div class="h-[40rem] w-full">
+				<img
+					in:fade={{ duration: 400 }}
+					on:load={() => {
+						loaded = true;
+					}}
+					class="h-auto w-full {loaded ? 'fade-fast' : 'opacity-100'}"
+					src="/hand.png"
+					alt="01 light"
+				/>
+			</div>
+		</div>
+		<!-- RIGHT -->
+		<div class="w-full px-10 pt-10 md:w-1/2">
+			<div class="flex w-full justify-center">
+				<p class=" text-white">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro vero doloremque sit nulla
+					molestiae minus iste nam suscipit aspernatur facere numquam magnam, accusamus repellendus.
+					Quis eum possimus exercitationem nesciunt est? Lorem ipsum, dolor sit amet consectetur
+					adipisicing elit. Deserunt repellat blanditiis architecto est ut magni quaerat eum fugit
+					sapiente. Voluptatem excepturi debitis natus vero! Quos facere maxime amet quam similique!
+				</p>
+			</div>
+
+			<h2 class="mt-10 text-3xl text-white">Guides</h2>
+			<div class="flex w-full items-center gap-4">
+				<a
+					href="https://github.com/KillianLucas/01"
+					class="z-5 group relative mt-4 flex max-w-[16rem] cursor-pointer flex-col gap-1 border border-white px-3 py-2 text-white transition hover:bg-white hover:text-black"
+				>
+					<h3 class="text-lg font-semibold">Hardware</h3>
+					<p class="text-sm">Everything you need to build an 01 Light in an afternoon</p>
+					<Icon
+						icon="ci:arrow-up-right-md"
+						class="absolute right-1 top-1 h-6 w-6  text-white group-hover:text-black"
+					/>
+				</a>
+				<a
+					href="https://github.com/KillianLucas/01"
+					class="z-5 group relative mt-4 flex h-full max-w-[16rem] cursor-pointer flex-col gap-1 border border-white px-3 py-2 text-white transition hover:bg-white hover:text-black"
+				>
+					<h3 class="text-lg font-semibold">Software</h3>
+					<p class="text-sm">Get 01OS up and running on a variety of devices in no time</p>
+					<Icon
+						icon="ci:arrow-up-right-md"
+						class="absolute right-1 top-1 h-6 w-6  text-white group-hover:text-black"
+					/>
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="flex w-full flex-col md:flex-row">
+		<!-- LEFT -->
+		<div class="w-full md:w-1/2">
+			<p class="text-center">Limited stock of 100 units, order now</p>
+			<div class="flex w-full justify-center px-5">
+				<a
+					class="z-5 mt-4 h-min cursor-pointer rounded-full border border-black px-3 py-2 text-black transition hover:bg-black hover:text-white"
+					href="https://0ggfznkwh4j.typeform.com/to/UvODRiEO"
+					target="_blank"
+					rel="noopener noreferrer">Pre-Order</a
+				>
+			</div>
+		</div>
+		<!-- RIGHT -->
+		<div class="w-full md:w-1/2">
+			<div class="flex w-full flex-col items-end justify-end px-5">
+				<div class="w-fit">
+					<p class=" text-white">Community</p>
+					<div class=" flex items-center justify-center gap-4">
+						<a
+							class="z-5 mt-4 flex h-min cursor-pointer items-center gap-2 border border-white px-3 py-2 text-white transition hover:bg-white hover:text-black"
+							href="https://discord.gg/Hvz9Axh84z"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Icon icon="ic:round-discord" class="h-5 w-5" /> Discord
+						</a>
+						<a
+							class="z-5 mt-4 flex h-min cursor-pointer items-center gap-2 border border-white px-3 py-2 text-white transition hover:bg-white hover:text-black"
+							href="https://github.com/KillianLucas/01"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Icon icon="fa:github" class="h-5 w-5" /> Github
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
